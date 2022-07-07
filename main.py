480ef80edb48358ae54d725acb168627dbe4221b
Adrian Bibby Walther <adrianbibbywalther@gmail.com>
2022-07-07 20:35:49 +0200

    Now we can print KoD runs.

--- main.py
+++ main.py
@@ -1,66 +1,86 @@
-from board import Board
+# from board import Board
 from run import Run
 import srcomapi, srcomapi.datatypes as dt
-api = srcomapi.SpeedrunCom(); api.debug = 1
-f = open("kod any.txt", "r")
-
-importlist = []
-
-for x in f:
-    importlist.append(x.split("\t"))
-
-f.close()
-runlist = []
-for x in importlist:
-    runlist.append(Run(x[0],x[1]))
 
 
+def main():
+    api = srcomapi.SpeedrunCom()
+    api.debug = 1
 
-# unit tests
-# runs
-# print("\n # test of runs, not part of a leaderboard")
-# print(importlist[0][1])
-# for x in runlist:
-#     print(x)
-#
-# # boards
-# print("\n # test of boards")
-#
-# leaderboard = Board()
-# for x in runlist:
-#     leaderboard.addRun(x)
-#
-# print(leaderboard)
+    with open("kod any.txt", "r") as f:
+        importlist = [x.split("\t") for x in f]
+
+    runlist = [(Run(x[0], x[1])) for x in importlist]
+
+    # unit tests
+    # runs
+    # print("\n # test of runs, not part of a leaderboard")
+    # print(importlist[0][1])
+    # for x in runlist:
+    #     print(x)
+    #
+    # # boards
+    # print("\n # test of boards")
+    #
+    # leaderboard = Board()
+    # for x in runlist:
+    #     leaderboard.addRun(x)
+    #
+    # print(leaderboard)
+
+    # srcom api test
+    # doesnt fucking work
+
+    # gameid = "3dxz351y"
+
+    game = api.search(srcomapi.datatypes.Game, {"name": "dishonored"})[0]
+    dh1_runs = {}
+
+    dh1_runs[game.categories[1].name] = api.get(
+        f"leaderboards/{game.id}/"
+        f"category/{game.categories[1].id}?embed=variables,players,platform"
+    )
+
+    kod = dh1_runs["Knife of Dunwall"]
+
+    variables = kod["variables"]["data"]
+
+    assert len(variables) == 1
+    kod_category = variables[0]
+
+    players = {p["id"]: p for p in kod["players"]["data"]}
+
+    for run in kod["runs"]:
+        run = run["run"]
+
+        category_value = run["values"][kod_category["id"]]
+        label = kod_category["values"]["values"][category_value]["label"]
+
+        runner_id = run["players"][0]["id"]
+        runner_name = players[runner_id]["names"]["international"]
+
+        time = run["times"]["primary_t"]
+
+        # TODO: Also handle guests?
+        print(f"{label}: {runner_name}, {time}")
+
+    # print(kod.runs[1][comment])
+    # print(game.categories[1].runs)
+
+    # print("lmao " + str(game.categories[1]))
+    # print(" category test: " + str(game.categories[1].variables))
+    # list = []
+    # for x in game.runs:
+    #     if x.categories.get("Knife of Dunwall"):
+    #         list.append(x)
+    # print(x)
+    #
+    # print("asdf" + str(list))
+    # gameid = "3dxz351y"
+    # dh_runs = {}
+    #
+    # print(dh_runs)
 
 
-# srcom api test
-# doesnt fucking work
-
-gameid = "3dxz351y"
-
-game = api.search(srcomapi.datatypes.Game, {"name": "dishonored"})[0]
-dh1_runs = {}
-
-dh1_runs[game.categories[1].name] = dt.Leaderboard(api, data=api.get("leaderboards/{gameidentification}/category/{a1}?embed=variables".format(gameidentification = game.id, a1  = game.categories[1].id)))
-kod = dh1_runs["Knife of Dunwall"]
-print("1234 " + str(kod.variables))
-print(kod.variables)
-for x in kod.runs:
-    print(x["run"])
-
-#print(kod.runs[1][comment])
-#print(game.categories[1].runs)
-
-# print("lmao " + str(game.categories[1]))
-# print(" category test: " + str(game.categories[1].variables))
-# list = []
-# for x in game.runs:
-#     if x.categories.get("Knife of Dunwall"):
-#         list.append(x)
-# print(x)
-#
-# print("asdf" + str(list))
-# gameid = "3dxz351y"
-# dh_runs = {}
-#
-# print(dh_runs)
+if __name__ == "__main__":
+    main()
